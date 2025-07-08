0f41a64ed0f680e1a1e548d17bc0133065a69261
Phanideep31 <154726401+Phanideep31@users.noreply.github.com>
2025-07-08 18:30:39 +0530

    Update Navbar.jsx

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   const handleLogout = async () => {
     try {
       const response = await axios.get(
-        "http://localhost:4000/api/v1/user/logout",
+        "https://mern-backend-0jvv.onrender.com/api/v1/user/logout",
         {
           withCredentials: true,
         }
