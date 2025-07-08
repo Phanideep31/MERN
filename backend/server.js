55b80f306353b84a8226c3fd48a9abda6ff5c798
Phanideep31 <154726401+Phanideep31@users.noreply.github.com>
2025-07-08 18:50:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,6 @@ cloudinary.v2.config({
   api_secret: process.env.CLOUDINARY_API_SECRET, // CLOUDINARY_CLIENT_SECRET
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 4000, () => {
   console.log(`Server running at port ${process.env.PORT}`);
 });
