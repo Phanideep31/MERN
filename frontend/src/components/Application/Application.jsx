2fb9466c07fa1fddc1b6c2c07eda5d8f47a29cf4
Phanideep31 <154726401+Phanideep31@users.noreply.github.com>
2025-07-08 18:26:03 +0530

    Update Application.jsx

--- frontend/src/components/Application/Application.jsx
+++ frontend/src/components/Application/Application.jsx
@@ -72,7 +72,7 @@ const Application = () => {
 
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/application/post",
+        "https://mern-backend-0jvv.onrender.com/api/v1/application/post",
         formData,
         {
           withCredentials: true,
